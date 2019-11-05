b768a219a7b3c83fe023bdd00d7899860fa97f89
Andrew Chan <andrewchan@Andrew-MacBook.local>
2019-11-05 10:56:54 +0800

    first commit

--- apps/settings.py
+++ apps/settings.py
@@ -16,12 +16,12 @@ API_PREFIX = '/youdao/api/' + API_VERSION
 YOUDAO_OCR_URL = 'https://openapi.youdao.com/ocrapi'
 YOUDAO_TRANSLATE_URL = 'https://openapi.youdao.com/api'
 YOUDAO_ASR_URL = 'https://openapi.youdao.com/asrapi'
-YOUDAO_APP_KEY = '720c7860d9952239'
-YOUDAO_APP_SECRET = 'HSyinyj17TZQmpGiVhUnpvUhAxAXP975'
+YOUDAO_APP_KEY = 'appkey'
+YOUDAO_APP_SECRET = 'appsecret'
 
 BAIDU_IMAGE_RECOGNITION = 'https://aip.baidubce.com/rest/2.0/image-classify/v2/advanced_general'
-BAIDU_API_KEY = 'sTxpG3RVcIWBf0P2xGvYYju9'
-BAIDU_API_SECRET = 'i2KUysGwnwWQQg2vZCbrzgs0x050Clzb'
+BAIDU_API_KEY = 'apikey'
+BAIDU_API_SECRET = 'apisecret'
 
 
 CODE_OK = 0
